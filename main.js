82786b9235fc397d140cbb2cf46b0061c6156fdf
Justas Brazauskas <brazauskasjustas@gmail.com>
2013-12-30 14:30:43 +0200

    First version

---
+++ main.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+var util = require('util');
+var copier = require('./lib/copier');
+var EventEmitter = require('events').EventEmitter;
+
+module.exports = rcp;
+
+function rcp(options) {
+    this.limit = options.limit || 32;
+    this.stopOnError = options.stopOnError || false;
+}
+util.inherits(rcp, EventEmitter);
+
+rcp.prototype.copy = function(source, destination, callback) {
+    var self = this;
+    options = {
+        limit: this.limit,
+        stopOnError: this.stopOnError
+    };
+    copier = new copier(source, destination, options, function(err, processed) {
+        if(err) {
+            callback(err);
+            return;
+        }
+        callback(null, processed);
+    });
+    copier.on('error', function(err) {
+        if(self.stopOnError) {
+            callback(err);
+        } else {
+            self.emit('error', err);
+        }
+    });
+}
